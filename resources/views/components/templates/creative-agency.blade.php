b4f1959aff776cf94252e4967b920efdfe7046da
Faustino Vasquez <vasquez.limon@gmail.com>
2026-02-18 16:00:39 -0800

    creative-agengy almost complete

---
+++ resources/views/components/templates/creative-agency.blade.php
@@ -0,0 +1,12 @@
+@props(['invoice', 'forPdf' => false])
+
+@php
+    $primaryColor = $invoice->template->settings['primary_color'] ?? '#7c3aed';
+@endphp
+
+<div class="creative-agency bg-white" style="min-height: 297mm;">
+    {{-- Colored Header Bamnner --}}
+    {{-- 10:16 --}}
+
+
+</div>
